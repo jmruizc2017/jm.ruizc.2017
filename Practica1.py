dfb2678f106a1a1a45fd46706d30f692180e1744
jmruizc2017 <60428521+jmruizc2017@users.noreply.github.com>
2020-01-29 12:54:19 +0100

    Update Practica1.py

--- Practica1.py
+++ Practica1.py
@@ -6,3 +6,12 @@ file = "/etc/passwd"
 file_object = open(file, 'r')
 lines = file_object.readlines()
 print(lines)
+lines.split(',')
+
+file = "/etc/passwd"
+>>> lines = file_object.readlines()
+>>> file = "/etc/passwd"
+>>> file_object = open(file, 'r')
+>>> lines = file_object.read()
+>>> print(lines)
+
